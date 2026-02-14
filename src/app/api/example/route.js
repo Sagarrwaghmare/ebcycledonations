b4ae4072a5bd66c9ded31927d25ad5055fc5ff88
Sagarrwaghmare <sagarwaghmare587@gmail.com>
2026-02-14 19:31:55 +0530

    first commit

---
+++ src/app/api/example/route.js
@@ -0,0 +1,4 @@
+export async function GET(){
+    const data = {name:"Example", id:"ROUTE"}
+    return Response.json(data)
+}
---
+++ src/app/example/page.jsx
@@ -0,0 +1,15 @@
+
+export default async function Example(){
+    // let res = await fetch("http://localhost:3000/api/example",{cache:'no-store'})
+    // console.log(res.json)
+    // let data = res.json()
+
+    // console.log(data); 
+    return (
+        <main>
+            <h1 className="text-blue-500 ">HELLOooo</h1>
+
+            {/* <h2>{JSON.stringify(data, null, 2)}</h2> */}
+        </main>
+    )
+}
